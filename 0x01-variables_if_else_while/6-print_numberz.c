fe8c16e9ee6c3bae047e5a699e577234e5598554
devMantse <devmantse@gmail.com>
2022-12-11 01:09:01 +0000

    scripts

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digits of base 10
+ *
+ * Return: Return 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
